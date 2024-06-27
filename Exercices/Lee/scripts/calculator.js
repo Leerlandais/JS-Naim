812e3a7f2a324e622ff58ed27c0ed0c6e8c820f8
leerlandais <leerlandais@gmail.com>
2024-06-27 07:45:25 +0100

    calc low coded and prettier

--- Exercices/Lee/scripts/calculator.js
+++ Exercices/Lee/scripts/calculator.js
@@ -1,20 +1,20 @@
 const calcWindow = document.getElementById("calcWindow");
 const button = document.querySelectorAll('button');
+const classForButton = `bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded`
 
-console.log(button.length);
-
+for (let i = 0;i < button.length; i++) {
+    button[i].className = classForButton;
+}
+// one function to clear them all
 function clearResult() {
     calcWindow.value = "";
 }
-
+// one function to bind them
 function appendCharacter(btnPressed) {
     calcWindow.value += btnPressed;
 }
 
-
-// afin de reduire le code au plus petit possible, j'ai utiliser le fonction eval()
-// if faut jamais utiliser ce fonction car il est très ouvert aux attaques
-// quand j'ai le temps, je ferai un calc sans utilisant eval()
+// one function to bring them all, and in the darkness, eval() them
 function calculateResult() {
     let result =
         calcWindow.value;
@@ -25,3 +25,6 @@ function calculateResult() {
         calcWindow.value = "Something went wrong";
     }
 }
+// afin de reduire le code au plus petit possible, j'ai utiliser le fonction eval()
+// if faut jamais utiliser ce fonction car il est très ouvert aux attaques
+// quand j'ai le temps, je ferai un calc sans utilisant eval()
